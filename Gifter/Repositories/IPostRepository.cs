7864de20f247a444bbd4e5ccd42ffcc12556cefe
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-03-10 10:36:35 -0500

    push up code

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Gifter.Models;
+using Gifter.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        void Add(Post post);
+        void Delete(int id);
+        List<Post> GetAll();
+        List<Post> GetAllWithComments();
+        Post GetById(int id);
+        void Update(Post post);
+    }
+}
